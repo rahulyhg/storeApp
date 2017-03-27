ef5bd7d9e18ae7e17f834ee5e33ae1dcdd19fd08
olegmxjack <olegmxjack@rambler.ru>
2017-03-27 11:57:19 +0300

    define const

--- public/index.php
+++ public/index.php
@@ -2,3 +2,9 @@
 /**
  * Created by comrade
  */
+session_start();
+define('ROOT_DIR', __DIR__);
+define('APP_DIR', dirname(__DIR__));
+
+require_once implode(DIRECTORY_SEPARATOR, [APP_DIR, 'vendor', 'autoload']).'.php';
+
