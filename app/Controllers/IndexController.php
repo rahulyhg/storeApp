f2c73fa1563a87f442d3d3f740d262ce73209c24
olegmxjack <olegmxjack@rambler.ru>
2017-03-27 14:43:21 +0300

    Run app

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -1,9 +1,15 @@
 <?php
 
 namespace comrade\Controllers;
+use DI\ContainerBuilder;
+use DI\Bridge\Slim\App as DiBridge;
 
-
-class IndexController
+class IndexController extends DiBridge
 {
-    
+    protected function configureContainer(ContainerBuilder $builder)
+    {
+        $builder->addDefinitions([
+            'setting.displayErrorDetails' => true,
+        ]);
+    }
 }
--- public/index.php
+++ public/index.php
@@ -3,8 +3,16 @@
  * Created by comrade
  */
 session_start();
+
+use \comrade\Controllers\IndexController;
+
 define('ROOT_DIR', __DIR__);
 define('APP_DIR', dirname(__DIR__));
 
 require_once implode(DIRECTORY_SEPARATOR, [APP_DIR, 'vendor', 'autoload']).'.php';
 
+
+
+$app = new IndexController();
+
+$app->run();
